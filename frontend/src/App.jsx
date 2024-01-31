15a87d09b86b09a4ae601488ed2142ab4a652a91
ShehalShah <102235406+ShehalShah@users.noreply.github.com>
2024-01-31 17:02:37 +0530

    fewf

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,20 @@
 import { useState } from 'react'
 import Home from './pages/Home'
+import { Routes, Route } from "react-router-dom";
+import LobbyScreen from "./pages/Lobby";
+import RoomPage from "./pages/Room";
+import { SocketProvider } from "./context/SocketProvider";
 
 function App() {
 
   return (
-    <Home />
+    <SocketProvider>
+      <Routes>
+      <Route path="/home" element={<Home />} />
+        <Route path="/" element={<LobbyScreen />} />
+        <Route path="/room/:roomId" element={<RoomPage />} />
+      </Routes>
+    </SocketProvider>
   )
 }
 
--- server/src/meeting/meeting.controller.ts
+++ server/src/meeting/meeting.controller.ts
@@ -1,7 +1,36 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Post } from '@nestjs/common';
 import { MeetingService } from './meeting.service';
+// const { SpacesServiceClient } = require('@google-cloud/meet').v2beta;
 
-@Controller('meeting')
-export class MeetingController {
-  constructor(private readonly meetingService: MeetingService) {}
-}
+// @Controller('meeting')
+// export class MeetingController {
+//   constructor(private readonly meetingService: MeetingService) {}
+
+//   @Post()
+//   async createMeet() {
+//     try {
+//       // Instantiates a client
+//       const meetClient = new SpacesServiceClient();
+
+//       // Construct request
+//       const request = {};
+
+//       // Run request
+//       const response = await meetClient.createSpace(request);
+      
+//       // Process the response or use it as needed
+//       console.log(response);
+
+//       // Additional logic to send the meeting link to the user's Gmail
+//       // You might want to call your MeetingService to handle this part
+//       // For example, this could involve sending an email to the user with the meeting link.
+//       // const meetingLink = response.someProperty; // Extract the meeting link from the response
+//       // await this.meetingService.sendMeetingLinkToUser(userEmail, meetingLink);
+
+//       return { message: 'Meeting created successfully' };
+//     } catch (error) {
+//       console.error('Error creating meeting:', error);
+//       throw new Error('Failed to create meeting');
+//     }
+//   }
+// }
