9671379fa9c4cca1cdfccc394af60a06998537d4
ShehalShah <102235406+ShehalShah@users.noreply.github.com>
2024-01-31 11:44:30 +0530

    front

---
+++ frontend/src/pages/Home.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Sidebar from '../components/Sidebar'
+
+const Home = () => {
+  return (
+
+    <div className='w-screen h-screen bg-[#020617] text-white'>
+    <Sidebar />
+        HOME
+    </div>
+  )
+}
+
+export default Home
